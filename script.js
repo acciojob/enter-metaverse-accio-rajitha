1bbcade531791d58210a4777b32a484c60491c36
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-30 23:40:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+var para = document.getElementById("status");
+var enterbutton = document.getElementById("enterBtn");
+
+function textstatus(){
+	para.textContent = "Entered Metaverse"
+}
+enterbutton.addEventListener('click', textstatus);
